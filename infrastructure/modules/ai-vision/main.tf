9a1ddefee22e6c792d1ff18d4cc26c64687f1fd6
unknown <piotr.brodzinski@luxmed.pl>
2025-03-28 20:50:58 +0100

    rename folders

--- infrastructure/modules/ai-vision/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_cognitive_account" "vision-ai-service" {  
-  name                = "vision-ai-service"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku_name            = "F0"
-  kind                = "ComputerVision" # other kinds: ComputerVision, Face, Emotion, FromRecognizer, TextAnalytics etc.
-}
