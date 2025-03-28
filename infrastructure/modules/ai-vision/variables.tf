3f4c5a54e6ecec5c91a44fea7db4d9e8b17c54f9
unknown <piotr.brodzinski@luxmed.pl>
2025-03-28 20:46:13 +0100

    restored infra

---
+++ infrastructure/modules/ai-vision/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group."
+  type = string  
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources will be created."
+  type = string  
+}
