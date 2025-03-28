8fecc63ac5d8b0e861a08433641c29bf5db662d8
bridzinppp <145057439+bridzinppp@users.noreply.github.com>
2025-03-28 20:51:51 +0100

    rename folders (#11)
    Co-authored-by: unknown <piotr.brodzinski@luxmed.pl>

--- infrastructure/modules/ai-search/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_search_service" "ai-search" {  
-  name                = "ai-search-pb1980"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = "free"
-  local_authentication_enabled = true
-  authentication_failure_mode  = "http403"
-}
--- infrastructure/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  description = "The Azure Region in which all resources will be created."
-  type = string  
-}
-
-variable "rg-name" {
-  description = "The name of the Azure Resource Group."
-  type = string  
-}
