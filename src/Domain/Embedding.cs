25f9d604cd8544f6da4bcf34814080e68caf9ff8
unknown <piotr.brodzinski@luxmed.pl>
2025-04-26 18:05:55 +0200

    little fix

--- src/Domain/Embedding.cs
+++ src/Domain/Embedding.cs
@@ -4,7 +4,7 @@
     {
         public required string FileName { get; init; }
         public int Page { get; init; }
-        public string pageContent { get; set; } = string.Empty;
+        public string PageContent { get; set; } = string.Empty;
         public IEnumerable<float> Vectors { get; set; } = new List<float>();
     }
 }
