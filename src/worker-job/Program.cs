2d3231fdd2c96049b82b396538ddff28e43e310d
bridzinppp <145057439+bridzinppp@users.noreply.github.com>
2025-05-10 21:47:08 +0200

    added kv reading secrets (#28)
    Co-authored-by: unknown <piotr.brodzinski@luxmed.pl>

--- src/worker-job/Program.cs
+++ src/worker-job/Program.cs
@@ -8,21 +8,24 @@ namespace worker_job
         static async Task Main(string[] args)
         {
             // Replace with your Key Vault URL and secret name
-            string keyVaultUrl = "https://keyvault-ai-pb-1980.vault.azure.net/";
-            string secretName = "ai-search-key";
+            //string keyVaultUrl = "https://keyvault-ai-pb-1980.vault.azure.net/";
+            string secretName = "aisearchkey";
+            // read value from env\
+            Console.WriteLine(Environment.GetEnvironmentVariable(secretName));
 
-            // Authenticate using DefaultAzureCredential
-            var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
 
-            try
-            {
-                KeyVaultSecret secret = await client.GetSecretAsync(secretName);
-                Console.WriteLine($"Secret value: {secret.Value}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error retrieving secret: {ex.Message}");
-            }
+            //// Authenticate using DefaultAzureCredential
+            //var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+            //try
+            //{
+            //    KeyVaultSecret secret = await client.GetSecretAsync(secretName);
+            //    Console.WriteLine($"Secret value: {secret.Value}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Error retrieving secret: {ex.Message}");
+            //}
             await Console.Out.WriteLineAsync("Hello World!");
         }
     }
